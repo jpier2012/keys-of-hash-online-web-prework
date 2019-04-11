f41e713ab9df34d786c443e711b8b5e8d0650118
James Pier <jpier2012@gmail.com>
2019-04-11 15:18:47 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,9 @@
 class Hash
   def keys_of(*args)
+  new_array = []
     args.each {|arg, value|
-      if animals.values.include?(value) }
+      if animals.values.include?(value)
+        
+    }
   end
 end
