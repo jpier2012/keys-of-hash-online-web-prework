264394c6fe93731b4c4e4f32ccabb271fb2e2a0b
James Pier <jpier2012@gmail.com>
2019-04-11 15:27:36 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,4 +1,7 @@
+
+def animals
 animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
+end
 
 class Hash
   def keys_of(*args)
