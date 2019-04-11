1f79223e6a2ee151786efa83a39a4b4ddb88fbe2
James Pier <jpier2012@gmail.com>
2019-04-11 15:25:08 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,8 +1,8 @@
 class Hash
   def keys_of(*args)
-    
+
   animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
-  
+
   new_array = []
     args.each {|arg|
       animals.each {|animal, location|
