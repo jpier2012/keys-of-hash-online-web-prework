8a82c99ddf742c3a4c74894b635476d75bde869f
James Pier <jpier2012@gmail.com>
2019-04-11 15:25:04 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,13 +1,17 @@
 class Hash
-  def keys_of(hash, *args)
+  def keys_of(*args)
+    
+  animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
+  
   new_array = []
     args.each {|arg|
-      hash.each {|animal, location|
+      animals.each {|animal, location|
         if location == arg
           new_array << animal
         end
       }
     }
   new_array
+  binding.pry
   end
 end
